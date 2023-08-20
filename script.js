cb2eb926003a4e4a39e8eb45144d514da051e29b
cool-master <118312584+cool-master@users.noreply.github.com>
2023-08-21 02:52:14 +0300

    script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ sendButton.addEventListener('click', () => {
     inputField.value = '';
 
     // Save the message to a file on the server.
-    fetch('http://localhost:8080/messages', {
+    fetch('http://cool-master.github.io:8080/messages', {
       method: 'POST',
       body: JSON.stringify({
         message: message
@@ -36,7 +36,7 @@ sendButton.addEventListener('click', () => {
 });
 
 // When the page loads, get all the messages from the server.
-fetch('http://localhost:8080/messages')
+fetch('http://cool-master.github.io:8080/messages')
   .then(response => response.json())
   .then(messages => {
     // Add the messages to the chat box.
