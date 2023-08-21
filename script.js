036d8a8cdf8e16b2073c60e57d744751f5cc2817
cool-master <118312584+cool-master@users.noreply.github.com>
2023-08-21 03:10:26 +0300

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// This is the chat application.
-
-// Initialize the chat box.
 const chatBox = document.querySelector('.chat-box');
 
 // Initialize the input box.
@@ -27,66 +24,5 @@ sendButton.addEventListener('click', () => {
     inputField.value = '';
 
     // Save the message to a file on the server.
-    fetch('http://cool-master.github.io:8080/messages', {
-      method: 'POST',
-      body: JSON.stringify({
-        message: message
-      })
-    });
+    appendFile('chat.txt', message);
 });
-
-// When the page loads, get all the messages from the server.
-fetch('http://cool-master.github.io:8080/messages')
-  .then(response => response.json())
-  .then(messages => {
-    // Add the messages to the chat box.
-    messages.forEach(message => {
-      const messageElement = document.createElement('li');
-      messageElement.className = message.user === 'me' ? 'message me' : 'message you';
-      messageElement.textContent = message.message;
-      chatBox.appendChild(messageElement);
-    });
-      
-// This function maps the messages to their respective users.
-const mappedMessages = messages.map(message => ({
-  message: message.message,
-  user: message.user
-}));
-
-// This function displays the messages in the chat box.
-mappedMessages.forEach(message => {
-  const messageElement = document.createElement('li');
-  messageElement.className = message.user === 'me' ? 'message me' : 'message you';
-  messageElement.textContent = message.message;
-  chatBox.appendChild(messageElement);
-});
-
-const chatBox = document.querySelector('.chat-box');
-
-// Initialize the input box.
-const inputBox = document.querySelector('.input-box');
-
-// Initialize the input field.
-const inputField = inputBox.querySelector('input');
-
-// Initialize the send button.
-const sendButton = inputBox.querySelector('button');
-
-// When the send button is clicked, send the message.
-sendButton.addEventListener('click', () => {
-    // Get the message from the input field.
-    const message = inputField.value;
-
-    // Add the message to the chat box.
-    const messageElement = document.createElement('li');
-    messageElement.className = message.user === 'me' ? 'message me' : 'message you';
-    messageElement.textContent = message;
-    chatBox.appendChild(messageElement);
-
-    // Clear the input field.
-    inputField.value = '';
-
-    // Log the message to a file.
-    log("This is a message.", "chat-logs.txt");
-});
-  });
