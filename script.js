6dd53c99aa6b4999408c8435acf3e9e0eebbf6a1
cool-master <118312584+cool-master@users.noreply.github.com>
2023-08-21 03:07:08 +0300

    script.js

--- script.js
+++ script.js
@@ -61,5 +61,32 @@ mappedMessages.forEach(message => {
   chatBox.appendChild(messageElement);
 });
 
-      log("This is a message.", "chat-logs.txt");
+const chatBox = document.querySelector('.chat-box');
+
+// Initialize the input box.
+const inputBox = document.querySelector('.input-box');
+
+// Initialize the input field.
+const inputField = inputBox.querySelector('input');
+
+// Initialize the send button.
+const sendButton = inputBox.querySelector('button');
+
+// When the send button is clicked, send the message.
+sendButton.addEventListener('click', () => {
+    // Get the message from the input field.
+    const message = inputField.value;
+
+    // Add the message to the chat box.
+    const messageElement = document.createElement('li');
+    messageElement.className = message.user === 'me' ? 'message me' : 'message you';
+    messageElement.textContent = message;
+    chatBox.appendChild(messageElement);
+
+    // Clear the input field.
+    inputField.value = '';
+
+    // Log the message to a file.
+    log("This is a message.", "chat-logs.txt");
+});
   });
